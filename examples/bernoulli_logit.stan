3f8961d16400a76e28314ac8a4de85906c4c672f
n-kall <n-kall@github.com>
2022-10-25 15:40:28 +0300

    add separation cmdstanr

---
+++ examples/bernoulli_logit.stan
@@ -0,0 +1,27 @@
+data {
+
+  int<lower=1> N; // total number of observations
+  array[N] int Y; // response variable
+  int<lower=1> K; // number of population-level effects
+  matrix[N, K] X; // population-level design matrix
+  array[K] real sigma; // sigma for priors
+}
+parameters {
+  vector[K] b; // population-level effects
+}
+transformed parameters {
+  real log_prior = 0; // prior contributions to the log posterior
+  log_prior += student_t_lpdf(b | 4, 0, sigma);
+}
+model {
+  // likelihood including constants
+    target += bernoulli_logit_glm_lpmf(Y | X, 0, b);
+  // priors including constants
+  target += log_prior;
+}
+generated quantities {
+  vector[N] log_lik;
+  for (n in 1:N) {
+    log_lik[n] = bernoulli_logit_lpmf(Y[n] | X[n] * b);
+  }
+}
---
+++ examples/separation.R
@@ -0,0 +1,86 @@
+library(tidyverse)
+library(brms)
+library(priorsense)
+library(cmdstanr)
+library(posterior)
+library(detectseparation)
+library(purrr)
+library(patchwork)
+library(ncomplete)
+
+m <- cmdstan_model(stan_file = "bernoulli_logit.stan")
+
+separate_gendata <- function(n = 15, intercept = 0, b = c(1, 1),
+                             sigma = c(5, 5, 5),
+                             lim = 1) {
+  X <- matrix(c(rep(1, n), runif(n * length(b), -lim, lim)),
+              ncol = length(b) + 1)
+  p <- plogis(rowSums(c(0, b) * X))
+  y <- rbinom(n, 1, p)
+  dat <- list(N = n, Y = y, X = X, K = ncol(X), sigma = sigma)
+
+  return(dat)
+}
+
+datasets <- map(
+  1:1000,
+  ~separate_gendata(
+    b = c(1, 1),
+    sigma = c(10, 10, 10)
+  )
+)
+
+sep <- map(datasets, ~detect_separation(x = .x$X, y = .x$Y)$separation)
+
+ncompl <- function(dataset) {
+
+  d <- matrix(c(dataset$X[, -1], dataset$Y), nrow = nrow(dataset$X))
+  ncomplete.for(d)
+
+}
+
+fits <- map(datasets, ~m$sample(data = .x, refresh = 0))
+
+sens <- map(fits, ~powerscale_sensitivity(.x))
+
+ncompletes <- map(datasets, ~ncompl(.x)$NCOMPLETE)
+
+prior_sens <- map(sens, function(s) {s$sensitivity %>% select(prior) %>% max()})
+
+like_sens <- map(sens, function(s) {s$sensitivity %>% select(likelihood) %>% min()})
+
+pcomp <- qplot(unlist(ncompletes), unlist(prior_sens)) + geom_hline(yintercept = 0.05) + ylab("Prior sensitivity") + xlab("n_complete")
+
+lcomp <- qplot(unlist(ncompletes), unlist(like_sens)) + geom_hline(yintercept = 0.05) + ylab("Likelihood sensitivity") + xlab("n_complete")
+
+plt <- function(dataset) {
+
+  x1 <- dataset$X[,2]
+  x2 <- dataset$X[,3]
+  y <- dataset$Y
+
+  d <- tibble(x1 = x1, x2 = x2, y = y)
+  
+  d |>
+    ggplot(
+    aes(x = x1, y = x2, color = factor(y))) +
+    geom_point() +
+    coord_equal() +
+    xlim(-1, 1) +
+    ylim(-1, 1) +
+    theme_minimal() +
+    scale_color_brewer(type = "qual", palette = "Set1")
+}
+
+plots <- map(datasets, ~plt(.x))
+
+
+
+flagged <- map_lgl(sens, ~(!all(.x$sensitivity$diagnosis == "-")))
+flagged_sep <- intersect(which(flagged), which(unlist(sep)))
+flagged_nosep <- setdiff(which(flagged), which(unlist(sep)))
+unflagged <- which(!flagged)
+
+p <- wrap_plots(plots[c(unflagged[c(1)], flagged_sep[c(1)], flagged_nosep[c(1)])]) + plot_layout(guides = "collect", nrow = 1) + plot_annotation(tag_levels = "A")
+
+ggsave("sep.pdf", p, width = 8, height = 6)
